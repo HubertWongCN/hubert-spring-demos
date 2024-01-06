7ae9ad1f4ca1c643afbe8d6098be80db9b4675d5
hubertwong <hubertwongcn@163.com>
2024-01-06 16:24:03 +0800

    SpringBoot scheduled tasks - Spring Schedule implementation method

---
+++ springboot-demo-schedule-springtasks/src/main/java/org/hubert/springboot/springtasks/Application.java
@@ -0,0 +1,15 @@
+package org.hubert.springboot.springtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hubertwong
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
