aeaa1a5146b2ceb955d3802f92a55cd594981ec0
hubertwong <hubertwongcn@163.com>
2024-01-06 09:37:57 +0800

    SpringBoot database management - database management and migration using flyway

---
+++ springboot-demo-flyway-mysql/src/main/resources/db/migration/V1.1__Init_Data.sql
@@ -0,0 +1,5 @@
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+INSERT INTO `tb_user` VALUES (1,'hubert','sdfdfsd','hubert.org@gmail.com',1212121213,'afsdfsaf','2024-01-06 09:02:23','2024-01-06 09:02:23');
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
