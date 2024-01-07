00a5cb36a9ca591216357be9a38fd5cee5c8fd3a
hubertwong <hubertwongcn@163.com>
2024-01-08 02:29:47 +0800

    SpringBoot integration file - export PDF integrating itextpdf

---
+++ springboot-demo-helloworld-build-war/src/main/java/org/hubert/springboot/helloworld/build/war/Application.java
@@ -0,0 +1,34 @@
+package org.hubert.springboot.helloworld.build.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    /**
+     * hello world
+     *
+     * @return result
+     */
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("hello world", HttpStatus.OK);
+    }
+}
