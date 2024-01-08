aa6add82dc7adb67e4d5bf37081047dcdb454697
hubertwong <hubertwongcn@163.com>
2024-01-08 10:05:34 +0800

    SpringBoot monitoring - integrated springboot admin monitoring tool

---
+++ springboot-demo-admin/src/main/java/org/hubert/spirngboot/admin/Application.java
@@ -0,0 +1,20 @@
+package org.hubert.spirngboot.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author hubertwong
+ */
+@Configuration
+@EnableAdminServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
